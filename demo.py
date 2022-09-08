1f9f871ec4963a464b8995c2ae500225d3169148
nandhakumars4757 <113059991+nandhakumars4757@users.noreply.github.com>
2022-09-09 04:13:56 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -79,7 +79,7 @@ if content:
     transform = set_transform(content)
 process = st.button("Next Batch")
 image_batch = torch.stack(10 * [image])
-image_batch.to(device)
+
 transformeds = None
 try:
     transformeds = transform(image_batch)
