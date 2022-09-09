8baea5b2656d87eadf5d2cc68876018f78923a1d
nandhakumars4757 <113059991+nandhakumars4757@users.noreply.github.com>
2022-09-09 16:13:57 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -134,7 +134,7 @@ if transformeds is not None:
         i = i % 4
         cols[i].image(F.to_pil_image(x), use_column_width=True)
 
-st.success('QR code generated successfully!', icon= "✅")
+st.success('QR code generated successfully!')
 
 
 
